c7ee3dc9a8077c1ae94b864632661f619b1e153d
Svbudygin <145099561+Svbudygin@users.noreply.github.com>
2023-12-01 19:12:45 +0300

    Update links.py

--- links.py
+++ links.py
@@ -1,14 +1,14 @@
-DataCleanup = "/Users/sergeybudygin/PycharmProjects/pythonProject1/project_of_car_prices/static/texts/DataCleanup"
-gr1_overview = "/Users/sergeybudygin/PycharmProjects/pythonProject1/project_of_car_prices/static/texts/gr1+overview"
-data_old = '/Users/sergeybudygin/PycharmProjects/pythonProject1/project_of_car_prices/all_regions.csv'
-data = '/Users/sergeybudygin/PycharmProjects/pythonProject1/project_of_car_prices/output.csv'
+DataCleanup = "static/texts/DataCleanup"
+gr1_overview = "static/texts/gr1+overview"
+data_old = 'all_regions.csv'
+data = 'output.csv'
 paintings = [None]*20
 for i in range(1, 13):
-    paintings[i] = f'/Users/sergeybudygin/PycharmProjects/pythonProject1/project_of_car_prices/static/imges/{i}.png'
-intro = '/Users/sergeybudygin/PycharmProjects/pythonProject1/project_of_car_prices/static/texts/Intro'
-gr4 = "/Users/sergeybudygin/PycharmProjects/pythonProject1/project_of_car_prices/static/texts/gr4"
-gr7 = '/Users/sergeybudygin/PycharmProjects/pythonProject1/project_of_car_prices/static/texts/gr7'
-gr8 = '/Users/sergeybudygin/PycharmProjects/pythonProject1/project_of_car_prices/static/texts/gr8'
-gr9 = '/Users/sergeybudygin/PycharmProjects/pythonProject1/project_of_car_prices/static/texts/gr9'
-gr10 = '/Users/sergeybudygin/PycharmProjects/pythonProject1/project_of_car_prices/static/texts/gr10'
-gr56 = '/Users/sergeybudygin/PycharmProjects/pythonProject1/project_of_car_prices/static/texts/gr56'
+    paintings[i] = f'static/imges/{i}.png'
+intro = 'static/texts/Intro'
+gr4 = "static/texts/gr4"
+gr7 = 'static/texts/gr7'
+gr8 = 'static/texts/gr8'
+gr9 = 'static/texts/gr9'
+gr10 = 'static/texts/gr10'
+gr56 = 'static/texts/gr56'
