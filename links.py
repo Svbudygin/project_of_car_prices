e8af24c08b60c699dcb72cbda766450320da37fb
Svbudygin <145099561+Svbudygin@users.noreply.github.com>
2023-12-01 18:51:20 +0300

    Add files via upload

---
+++ links.py
@@ -0,0 +1,14 @@
+DataCleanup = "/Users/sergeybudygin/PycharmProjects/pythonProject1/project_of_car_prices/static/texts/DataCleanup"
+gr1_overview = "/Users/sergeybudygin/PycharmProjects/pythonProject1/project_of_car_prices/static/texts/gr1+overview"
+data_old = '/Users/sergeybudygin/PycharmProjects/pythonProject1/project_of_car_prices/all_regions.csv'
+data = '/Users/sergeybudygin/PycharmProjects/pythonProject1/project_of_car_prices/output.csv'
+paintings = [None]*20
+for i in range(1, 13):
+    paintings[i] = f'/Users/sergeybudygin/PycharmProjects/pythonProject1/project_of_car_prices/static/imges/{i}.png'
+intro = '/Users/sergeybudygin/PycharmProjects/pythonProject1/project_of_car_prices/static/texts/Intro'
+gr4 = "/Users/sergeybudygin/PycharmProjects/pythonProject1/project_of_car_prices/static/texts/gr4"
+gr7 = '/Users/sergeybudygin/PycharmProjects/pythonProject1/project_of_car_prices/static/texts/gr7'
+gr8 = '/Users/sergeybudygin/PycharmProjects/pythonProject1/project_of_car_prices/static/texts/gr8'
+gr9 = '/Users/sergeybudygin/PycharmProjects/pythonProject1/project_of_car_prices/static/texts/gr9'
+gr10 = '/Users/sergeybudygin/PycharmProjects/pythonProject1/project_of_car_prices/static/texts/gr10'
+gr56 = '/Users/sergeybudygin/PycharmProjects/pythonProject1/project_of_car_prices/static/texts/gr56'
